b251b4038c1be02def67eaa66073538176e701c6
Youssef Essam <youssefessam081@gmail.com>
2021-04-05 23:02:16 +0200

    coding

---
+++ code.js
@@ -0,0 +1,41 @@
+﻿
+
+formulanimator(document.querySelector('g'));
+
+
+
+
+myimg = document.querySelector("a");
+
+myimg.getAttribute("alt");
+
+
+
+
+
+/*    icon solor   */
+
+
+let el = document.querySelectorAll(".color-switcher li");
+let cL = [];
+
+
+
+
+for (let i = 0; i < el.length; i++) {
+
+    cL.push(el[i].getAttribute("data-color"));
+
+    el[i].addEventListener("click", function () {
+
+        document.body.classList.remove(...cL);
+
+        document.body.classList.add(this.getAttribute("data-color"));
+
+
+
+    }, false);
+}
+
+/*    icon solor   */
+
